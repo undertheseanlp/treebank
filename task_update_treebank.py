1614e0a088f689dea6233f70be2b9d4c7e1f7a63
Vu Anh <brother.rain.1024@gmail.com>
2017-06-08 16:51:15 +0700

    update treebank

--- task_update_treebank.py
+++ task_update_treebank.py
@@ -1,3 +1,4 @@
+from _csv import QUOTE_NONE
 from os.path import dirname, join, basename
 from os import listdir, remove
 import pandas as pd
@@ -18,29 +19,31 @@ def transform_line(input_file, output_file):
                 outfile.writelines(line.replace(".\tCH", ".\tCH\n").decode("utf-8"))
 
 
-def check_missing_token(corpus):
+def check_missing_token(corpus, file_id):
     """ ensure all tokens is tagging
 
     :type corpus: DataFrame 
     """
     for i in range(corpus.shape[0] - 1):
-        d = corpus.ix[i + 1, "start"] - corpus.ix[i, "end"]
+        x = corpus.ix[i + 1, "start"]
+        y = corpus.ix[i, "end"]
+        d = x - y
         if d != 1 and d != 2:
-            raise Exception("Error in Check Missing Token")
+            raise Exception("Check Missing Token error in file %s, position <end: %d, next: %d>" % (file_id, y, x))
     print "Check missing token\t: Pass"
 
 
 def make_treebank_file(brat_annotation_file, treebank_folder="."):
-    df = pd.read_csv(brat_annotation_file, sep="\t", names=["id", "token", "word"])
+    df = pd.read_csv(brat_annotation_file, sep="\t", names=["id", "token", "word"], quoting=QUOTE_NONE)
     df["tag"] = df.apply(lambda row: row["token"].split(" ")[0], axis=1)
     df["start"] = df.apply(lambda row: int(row["token"].split(" ")[1]), axis=1)
     df["end"] = df.apply(lambda row: int(row["token"].split(" ")[2]), axis=1)
     df = df.sort_values("start").reset_index()
-    check_missing_token(df)
     temp_file = "tmp"
-    df.to_csv("tmp", sep="\t", header=False, index=False, encoding="utf-8", line_terminator="\n",
-              columns=["word", "tag"])
+    df.to_csv(temp_file, sep="\t", header=False, index=False, encoding="utf-8", line_terminator="\n",
+              columns=["word", "tag"], quoting=QUOTE_NONE)
     file_id = re.match("p_(.*).ann", basename(brat_annotation_file)).group(1)
+    check_missing_token(df, file_id)
     output_file = join(treebank_folder, "%s.conll" % file_id)
     transform_line(temp_file, output_file)
     remove(temp_file)
